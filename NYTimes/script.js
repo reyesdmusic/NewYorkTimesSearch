d7edce2c0ba69f10420d1b94cdf2ae4333cd0249
Anthony Osiol <funnynoun@gmail.com>
2020-03-14 13:09:59 -0400

    javaScript

--- NYTimes/script.js
+++ NYTimes/script.js
@@ -1,6 +1,40 @@
-
 $(document).ready(function() {
 
+var  search = $("#search");
+var records = $("#records");
+var startYear = $("#startYear");
+var endYear = $("#endYear");
 
+
+
+
+  $("#buttonSearch").on("click", function() {
+  }
+
+
+  $("#buttonClear").on("click", function() {
+  }
+
+  
+
+
+    //
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
+
+    //
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+    
+      
+
+      .then(function(response) {
+          handleResponse(response);
+        });
+        
+
+            
   
   })
