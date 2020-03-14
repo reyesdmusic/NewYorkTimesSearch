c004a9d3437ea22d41ed5b1bb622b627794271a1
Anthony Osiol <funnynoun@gmail.com>
2020-03-14 13:33:23 -0400

    "javaScript"

--- NYTimes/script.js
+++ NYTimes/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-var  search = $("#search");
+var search = $("#search");
 var records = $("#records");
 var startYear = $("#startYear");
 var endYear = $("#endYear");
@@ -21,20 +21,18 @@ var endYear = $("#endYear");
     //
     var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats";
 
-    //
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
 
-    
-      
-
-      .then(function(response) {
-          handleResponse(response);
-        });
-        
-
-            
+  var query = "coronavirus";
+  var apiKey = "dBq9oIoxZlVDH6y81HDxiwPbGxLA70Am"
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&api-key=" + apiKey
   
-  })
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    console.log(response.response.docs[0].headline.main);
+    console.log(response.response.docs[0].abstract);
+    console.log(response.response.docs[0].pub_date);
+    console.log(response.response.docs[0].web_url);
+  });
